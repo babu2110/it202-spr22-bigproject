9abd5425e6d4f5c5b5e6118da033ea4117a125f6
Batbuyan Nyamdavaa <bnyamd2@uic.edu>
2022-04-29 13:22:06 -0500

    caching has been added

--- sw.js
+++ sw.js
@@ -1,15 +1,34 @@
+// Establish a cache name
+const cacheName = 'TNPcache';
+let urlsToCache = [
+  '/'
+]
+
 self.addEventListener('install', function(event) {
+  //perform install steps
   event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
+    caches.open(cacheName).then(function(cache) {
+      console.log("Opened cache")
       return cache.add('index.html');
     })
   );
 });
- 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  // Check if this is a navigation request
+  if (event.request.mode === 'navigate') {
+    // Open the cache
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      // Go to the network first
+      return fetch(event.request.url).then((fetchedResponse) => {
+        cache.put(event.request, fetchedResponse.clone());
+
+        return fetchedResponse;
+      }).catch(() => {
+        // If the network is unavailable, get
+        return cache.match(event.request.url);
+      });
+    }));
+  } else {
+    return;
+  }
 });
